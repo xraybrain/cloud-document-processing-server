99e54d8b8821fa232596548f385cfbc8dc158453
jude iwuji <xraybrain@gmail.com>
2021-11-01 20:46:49 +0100

    update

--- src/middlewares/uploadDocument.ts
+++ src/middlewares/uploadDocument.ts
@@ -4,6 +4,15 @@ import { NextFunction, Request, Response } from "express";
 import formidable, { File, IncomingForm } from "formidable";
 import Feedback from "../models/Feedback";
 import DocumentUpload from "../models/interfaces/DocumentUpload.interface.";
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
+  secure: true,
+});
 
 const MAX_FILE_SIZE = 50_000_000; // 50mb
 const defaultUploadDir = "../../public/uploads";
@@ -39,7 +48,7 @@ export const formidableService =
         }
 
         // Loop through upload files
-        uploadFiles.forEach((file, index) => {
+        uploadFiles.forEach(async (file, index) => {
           let tempPath = file.path;
           let document: DocumentUpload = {
             size: file.size,
@@ -61,34 +70,37 @@ export const formidableService =
             feedback.formData = fields;
             if (process.env.NODE_ENV === "production") {
               // Use cloudinary in prod
+              try {
+                let c = await cloudinary.uploader.upload(tempPath);
+                document.url = c.secure_url;
+                feedback.message += `${file.name} uploaded successfully.`;
+              } catch (error) {
+                feedback.message += `An error occured while uploading ${file.name}.`;
+              }
             } else {
-              let fileName = `${Date.now()}${path.extname(
-                file.name as string
-              )}`;
-              //-- upload file
-              fs.readFile(tempPath, (err, data) => {
-                if (err) throw err;
-                //-- rename fileToUpload
+              try {
+                let fileName = `${Date.now()}${path.extname(
+                  file.name as string
+                )}`;
+                //-- upload file
+                let data = fs.readFileSync(tempPath);
                 let newUploadDir = path.resolve(__dirname, uploadTo, fileName);
-                fs.writeFile(newUploadDir, data, (err) => {
-                  if (err) throw err;
-                  feedback.message += `${file.name} uploaded successfully`;
-                  document.url = `/${uploadTo.replace(
-                    /(\.\.\/)+?(public\/)?/g,
-                    ""
-                  )}/${fileName}`;
-                  feedback.results?.push(document);
-
-                  // call next middleware
-                  if (index + 1 == uploadFiles.length) {
-                    req.body = feedback;
-                    next();
-                  }
-                });
-              });
+                fs.writeFileSync(newUploadDir, data);
+                feedback.message += `${file.name} uploaded successfully`;
+                document.url = `/${uploadTo.replace(
+                  /(\.\.\/)+?(public\/)?/g,
+                  ""
+                )}/${fileName}`;
+                feedback.results?.push(document);
+              } catch (error) {
+                feedback.message += `An error occured while uploading ${file.name}.`;
+                console.log(error);
+              }
             }
           }
         });
+        req.body = feedback;
+        next();
       } else {
         feedback.success = false;
         feedback.message = "Missing 'upload' field";
